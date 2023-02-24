fa83fcaaf2dca7c51682f8a0afcbe0f14fbd1aed
mrodz <79176075+mrodz@users.noreply.github.com>
2023-02-23 23:08:46 -0800

    working on ImageCarousel

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from 'react'
 import Phone from '@mui/icons-material/Phone'
 import Button from '@mui/material/Button'
+
 import pizza from './pizza.webp'
+import wine from './wine.jpg'
+import parm from './parm.jpg'
+
 import './index.sass'
 
 function LocationBar() {
@@ -25,9 +29,66 @@ function HeaderButton({ content, emphasized = false }: HeaderButtonProps) {
 	)
 }
 
+interface ImageCarouselDotsProps {
+	len: number,
+	idx?: number
+}
+
+function ImageCarouselDots({ len, idx = 0 }: ImageCarouselDotsProps) {
+	function Dot(props: { on?: boolean }) {
+		return (
+			<div className="carousel-dot" {...props?.on && { "data-on": true }}>&bull;</div>
+		)
+	}
+
+	const dots = new Array(len)
+
+	for (let i = 0; i < len; i++) {
+		dots[i] = <Dot on={i === idx} />
+	}
+
+	return (
+		<div className="carousel-dots">
+			{dots}
+		</div>
+	)
+}
+
 interface ImageCarouselProps {
-	images: string[],
+	images: {
+		url: string,
+		description: string
+	}[],
 	startIdx?: number
+}
+
+const IMAGES = [
+	{
+		url: pizza,
+		description: 'This is pizza!'
+	},
+	{
+		url: wine,
+		description: 'Some of our specialty wines'
+	},
+	{
+		url: parm,
+		description: 'Check out our freshly-grated cheese'
+	}
+]
+
+function useRollover(size: number, initial: number = 0): [number, () => void, () => void] {
+	const [state, setState] = useState(initial)
+
+	function inc() {
+		setState(state => ++state > size ? 0 : state)
+	}
+
+	function dec() {
+		setState(state => --state < 0 ? size : state)
+	}
+
+	return [state, inc, dec]
 }
 
 /**
@@ -37,10 +98,13 @@ interface ImageCarouselProps {
  * BOTTOM = next()
  */
 function ImageCarousel({ images, startIdx = 0 }: ImageCarouselProps) {
-	const [{ currentIdx, nextIdx }, setIndexes] = useState({
-		currentIdx: startIdx,
-		nextIdx: undefined
-	})
+	type IndexesState = { currentIdx: number, nextIdx: number | undefined }
+
+	const [currentIdx, inc, dec] = useRollover(images.length - 1, startIdx)
+	// const [{ currentIdx, nextIdx }, setIndexes] = useState<IndexesState>({
+	// 	currentIdx: startIdx,
+	// 	nextIdx: undefined
+	// })
 
 	useEffect(() => {
 
@@ -55,16 +119,25 @@ function ImageCarousel({ images, startIdx = 0 }: ImageCarouselProps) {
 	}
 
 	return (
-		<div>
-			todo...
-		</div>
+		<div className="image-carousel landing-content-spacing">
+			<div role="img" style={{ backgroundImage: `url(${images[currentIdx].url})`, height: '100%', backgroundPosition: 'center top', backgroundSize: '100% auto' }}></div>
+
+			<div className="image-carousel-blurb important-left-items">
+				{images[currentIdx].description}
+			</div>
+
+			<button onClick={() => inc()}>++</button>
+			<button onClick={() => dec()}>--</button>
+
+			<ImageCarouselDots len={images.length} idx={currentIdx} />
+		</div >
 	)
 }
 
 function Header() {
 	return (
-		<div className='landing-header'>
-			<div className='header-logo'>
+		<div className='landing-header landing-content-spacing'>
+			<div className='header-logo important-left-items'>
 				Compari's
 			</div>
 			<nav id="header-items">
@@ -79,10 +152,15 @@ function Header() {
 export default function Landing() {
 	return (
 		<main className="landing">
-			<LocationBar />
-			<Header />
+			<section id="top">
+				<LocationBar />
+				<Header />
 
-			<ImageCarousel images={[]} />
+				<ImageCarousel images={IMAGES} />
+			</section>
+			<section>
+				tbd
+			</section>
 		</main>
 	)
 }
